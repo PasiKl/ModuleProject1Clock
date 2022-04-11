9a1847fab16d393ee22e204985f0dd4908797246
PasiKl <pasi.kleimola@gmail.com>
2022-04-11 11:26:58 +0300

    Add continuous seconds arm movement.

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 public class Clock : MonoBehaviour
 {
     [SerializeField]
+    bool continuous = false;
+
+    [SerializeField]
     Transform hoursPivot;
     [SerializeField]
     Transform minutesPivot;
@@ -22,11 +25,14 @@ public class Clock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        DateTime time = DateTime.Now;
-        TimeSpan timeSpan = time.TimeOfDay;
+        TimeSpan timeSpan = DateTime.Now.TimeOfDay;
 
         hoursPivot.localRotation = Quaternion.Euler(30f * (float)timeSpan.TotalHours, 0f, 0f);
         minutesPivot.localRotation = Quaternion.Euler(6f * (float)timeSpan.TotalMinutes, 0f, 0f);
-        secondsPivot.localRotation = Quaternion.Euler(6f * (float)timeSpan.Seconds, 0f, 0f);
+
+        if(continuous)
+            secondsPivot.localRotation = Quaternion.Euler(6f * (float)timeSpan.TotalSeconds, 0f, 0f);
+        else
+            secondsPivot.localRotation = Quaternion.Euler(6f * (float)timeSpan.Seconds, 0f, 0f);
     }
 }
